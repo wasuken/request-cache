a3dc74b4e3271f29b13e2fa5d54d22870bea48f2
wasuken <wevorence@gmail.com>
2023-10-29 16:41:28 +0900

    入力項目をreact-hook-formに置換

--- src/_components/InputInfoForm.tsx
+++ src/_components/InputInfoForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import styles from "./InputInfoForm.module.css";
+import { useForm } from "react-hook-form";
 
 interface IProps {
   handleSubmit: (
@@ -13,97 +14,105 @@ interface IProps {
 
 export default function InputInfoForm(props: IProps) {
   const { handleSubmit } = props;
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [url, setUrl] = useState<string>("");
-  const [timing, setTiming] = useState<number>(300);
+  const {
+    setValue,
+    register,
+    handleSubmit: handleFormSubmit,
+    formState: { errors },
+  } = useForm();
 
   const hhandleSubmit = () => {
     handleSubmit(title, description, url, timing);
-    setTitle("")
-    setDescription("")
-    setUrl("")
-    setTiming(300);
-  }
+    setValue("title", "");
+    setValue("description", "");
+    setValue("url", "");
+    setValue("timing", 300);
+  };
 
   return (
-    <div className={styles.formContainer}>
+    <form
+      className={styles.formContainer}
+      onSubmit={handleFormSubmit(handleSubmit)}
+    >
       <div className={styles.fieldGroup}>
-	<label htmlFor="title" className={styles.label}>
-	  タイトル
-	</label>
-	<input
-	  id="title"
-	  type="text"
-	  className={styles.inputField}
-	  value={title}
-	  onChange={(e) => setTitle(e.target.value)}
-	  maxLength={100}
-	/>
+        <label htmlFor="title" className={styles.label}>
+          タイトル
+        </label>
+        <input
+          id="title"
+          type="text"
+          className={styles.inputField}
+          maxLength={100}
+          {...register("title", { required: true })}
+        />
+      </div>
+      {errors.title && <div style={{ color: "red" }}>必須入力</div>}
+
+      <div className={styles.fieldGroup}>
+        <label htmlFor="description" className={styles.label}>
+          説明
+        </label>
+        <textarea
+          id="description"
+          className={styles.inputField}
+          maxLength={400}
+          {...register("description")}
+        ></textarea>
       </div>
 
       <div className={styles.fieldGroup}>
-	<label htmlFor="description" className={styles.label}>
-	  説明
-	</label>
-	<textarea
-	  id="description"
-	  className={styles.inputField}
-	  value={description}
-	  onChange={(e) => setDescription(e.target.value)}
-	  maxLength={400}
-	></textarea>
+        <label htmlFor="url" className={styles.label}>
+          URL
+        </label>
+        <input
+          id="url"
+          type="url"
+          className={styles.inputField}
+          {...register("url", { required: true })}
+        />
       </div>
+      {errors.url && <div style={{ color: "red" }}>必須入力</div>}
 
       <div className={styles.fieldGroup}>
-	<label htmlFor="url" className={styles.label}>
-	  URL
-	</label>
-	<input
-	  id="url"
-	  type="url"
-	  className={styles.inputField}
-	  value={url}
-	  onChange={(e) => setUrl(e.target.value)}
-	/>
+        <label htmlFor="timing" className={styles.label}>
+          取得タイミング（秒）
+        </label>
+        <input
+          id="timing"
+          type="number"
+          min="500"
+          className={styles.inputField}
+          {...register("timing", {
+            required: {
+              value: true,
+              message: "必須入力",
+            },
+            minLength: {
+              value: 300,
+              message: "300以上で入力してください",
+            },
+          })}
+        />
       </div>
+      {errors.timing && <div style={{ color: "red" }}>必須入力</div>}
 
       <div className={styles.fieldGroup}>
-	<label htmlFor="timing" className={styles.label}>
-	  取得タイミング（秒）
-	</label>
-	<input
-	  id="timing"
-	  type="number"
-	  min="500"
-	  className={styles.inputField}
-	  value={timing}
-	  onChange={(e) => setTiming(parseInt(e.target.value))}
-	/>
+        <label htmlFor="preset-timing" className={styles.label}>
+          プリセット(取得タイミング（秒）)
+        </label>
+        <select
+          id="preset-timing"
+          className={styles.selectField}
+          onChange={(e) => setValue("timing", parseInt(e.target.value))}
+        >
+          <option value="">選択してください</option>
+          <option value={604800}>一週間ごと</option>
+          <option value={86400}>一日ごと</option>
+          <option value={3600}>一時間ごと</option>
+        </select>
       </div>
 
-      <div className={styles.fieldGroup}>
-	<label htmlFor="preset-timing" className={styles.label}>
-	  プリセット
-	</label>
-	<select
-	  id="preset-timing"
-	  className={styles.selectField}
-	  onChange={(e) => setTiming(parseInt(e.target.value))}
-	>
-	  <option value="">選択してください</option>
-	  <option value={604800}>一週間ごと</option>
-	  <option value={86400}>一日ごと</option>
-	  <option value={3600}>一時間ごと</option>
-	</select>
-      </div>
-
-      <button
-	className={styles.button}
-	onClick={() => handleSubmit(title, description, url, timing)}
-      >
-	登録
-      </button>
-    </div>
+      <button className={styles.button}>登録</button>
+    </form>
   );
 }
--- src/_components/TimingList.tsx
+++ src/_components/TimingList.tsx
@@ -1,45 +1,115 @@
 "use client";
 // TimingList.tsx
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TimingList.module.css";
 import { Timing } from "@/_const/types";
 
 interface IProps {
-  title: string;
+  pageTitle: string;
   timings: Timing[];
+  onUpdate: (
+    id: number,
+    title: string,
+    description: string,
+    url: string,
+    timing: number
+  ) => Promise<void>;
 }
 
-const TimingList: React.FC<IProps> = ({ timings, title }) => {
+const TimingList: React.FC<IProps> = ({
+  timings,
+  pageTitle,
+  onUpdate,
+}: IProps) => {
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+  const [timing, setTiming] = useState<number>(300);
+  const handleEditClick = (i: number) => {
+    setEditIndex(i);
+    const tm: Timing = timings[i];
+    setTitle(tm.title);
+    setDescription(tm.description);
+    setUrl(tm.url);
+    setTiming(tm.get_timing_sec);
+  };
+  const endEdit = async () => {
+    const tm: Timing = timings[editIndex];
+    await onUpdate(tm.id, title, description, url, timing);
+    setEditIndex(null);
+  };
+  const cancelEdit = () => {
+    setEditIndex(null);
+  };
   return (
     <div className={styles.timingList}>
-      <h2 className={styles.title}>{title}</h2>
+      <h2 className={styles.title}>{pageTitle}</h2>
       <table className={styles.table}>
-	<thead>
-	  <tr>
-	    <th>タイトル</th>
-	    <th>説明</th>
-	    <th>URL</th>
-	    <th>取得タイミング（秒）</th>
-	  </tr>
-	</thead>
-	<tbody>
-	  {timings.map((timing) => (
-	    <tr key={timing.id}>
-	      <td>
-		{timing.title.length > 50
-		  ? timing.title.substring(0, 47) + "..."
-		  : timing.title}
-	      </td>
-	      <td>
-		{timing.description.length > 50
-		  ? timing.description.substring(0, 47) + "..."
-		  : timing.description}
-	      </td>
-	      <td>{timing.url}</td>
-	      <td>{timing.get_timing_sec}</td>
-	    </tr>
-	  ))}
-	</tbody>
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>説明</th>
+            <th>URL</th>
+            <th>取得タイミング（秒）</th>
+          </tr>
+        </thead>
+        <tbody>
+          {timings.map((tm, i) =>
+            i === editIndex ? (
+              <tr key={tm.id}>
+                <td>
+                  <button onClick={endEdit}>✔️</button>
+                  <button onClick={cancelEdit}>❌</button>
+                  <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    maxLength={100}
+                  />
+                </td>
+                <td>
+                  <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    maxLength={400}
+                  ></textarea>
+                </td>
+                <td>
+                  <input
+                    type="url"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    min="500"
+                    value={timing}
+                    onChange={(e) => setTiming(parseInt(e.target.value))}
+                  />
+                </td>
+              </tr>
+            ) : (
+              <tr key={tm.id}>
+                <td>
+                  <button onClick={() => handleEditClick(i)}>✏️</button>
+                  {tm.title.length > 50
+                    ? tm.title.substring(0, 47) + "..."
+                    : tm.title}
+                </td>
+                <td>
+                  {tm.description.length > 50
+                    ? tm.description.substring(0, 47) + "..."
+                    : tm.description}
+                </td>
+                <td>{tm.url}</td>
+                <td>{tm.get_timing_sec}</td>
+              </tr>
+            )
+          )}
+        </tbody>
       </table>
     </div>
   );
