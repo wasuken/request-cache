299096aa341e9761218943d0f8b4e310165a485f
wasuken <wasuken1@gmail.com>
2024-01-28 19:49:18 +0900

    fix api
    added pagination and var for pagination.

--- src/app/api/queues/result/[id]/route.ts
+++ src/app/api/queues/result/[id]/route.ts
@@ -5,22 +5,51 @@ const prisma = new PrismaClient();
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: number } },
 ) {
-  const { id } = params;
-  const queueResults = await prisma.urlInfoQueueResult.findMany({
-    where: {
-      urlInfo: {
-        id: parseInt(id),
-      },
+  const { id, page = "1", ppage = "10" } = params;
+  const ipage = parseInt(page);
+  const ippage = parseInt(ppage);
+  const skip = ipage * (ipage - 1);
+  const where = {
+    urlInfo: {
+      id: parseInt(id),
     },
+  };
+  const total = await prisma.urlInfoQueueResult.count({ where });
+  const totalPage = Math.ceil(total / ippage);
+  if (ipage > totalPage) {
+    return NextResponse.json(
+      {
+        errorMessage: `invalid parameters: page(${page}) > totalPage(${totalPage}).`,
+      },
+      { status: 400 },
+    );
+  }
+  if (ippage < 1 || ippage > 10) {
+    let msg = "";
+    if (ippage < 1) {
+      msg = `invalid parameters: invalid parameters: ppage < 1.`;
+    } else {
+      msg = `invalid parameters: invalid parameters: ppage > 10.`;
+    }
+    return NextResponse.json(
+      {
+        errorMessage: msg,
+      },
+      { status: 400 },
+    );
+  }
+  const queueResults = await prisma.urlInfoQueueResult.findMany({
+    where,
     include: {
       urlInfo: true,
     },
-    take: 10,
+    take: ippage,
+    skip,
     orderBy: {
       createdAt: "desc",
     },
   });
-  return NextResponse.json({ queueResults }, { status: 200 });
+  return NextResponse.json({ queueResults, totalPage, total }, { status: 200 });
 }
